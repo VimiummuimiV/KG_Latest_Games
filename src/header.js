83a2fddedb41eb7d07ca19d5eca6066de787c4cb
Aboba <blahblahmail.com>
2025-10-02 23:56:51 +0300

    Version down -1

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          KG_Latest_Games
 // @namespace     klavogonki
-// @version      1.7.4
+// @version      1.7.3
 // @description   Fast game creation buttons on all the pages
 // @match         *://klavogonki.ru/*
 // @author        Patcher
