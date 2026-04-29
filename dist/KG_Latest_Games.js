12a1f1eb925c7b4fd95114840871749f779f7de1
Aboba <blahblahmail.com>
2026-04-29 10:54:40 +0300

    Version 2.2.8

--- dist/KG_Latest_Games.js
+++ dist/KG_Latest_Games.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          KG_Latest_Games
 // @namespace     klavogonki
-// @version      2.2.7
+// @version      2.2.8
 // @description   Fast game creation buttons on all the pages
 // @match         *://klavogonki.ru/*
 // @author        Patcher
